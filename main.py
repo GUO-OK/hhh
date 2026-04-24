c02c3be3ef5d149f6efdae90773ccfca2dab5e4d
GUOSITONG <8209240319@csu.edu.cn>
2026-04-24 20:14:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main(page: ft.Page):
     page.padding = 0
     page.spacing = 0
     page.title = "乡旅与海E模式"
-    # 调用布局构建函数
+    
     # img = ft.Image(
     #     src="resource/img/test.png",
     #     width=400,
